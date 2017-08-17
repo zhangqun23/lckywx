70a33075a1ee66b786f2109abe76c97a7bf2dfa7
huiminjun <2802076526@qq.com>
2017-08-17 16:44:25 +0800

    王慧敏:班车部分添加时间筛选

--- src/com/mvc/entiy/BusTrade.java
+++ src/com/mvc/entiy/BusTrade.java
@@ -22,7 +22,7 @@ private String bune_bus;//车牌号
 private Integer bune_type;//交易类型，0表示线上，1表示线下
 private Integer invoice_if;//是否开发票，0:未开1:已开
 private String invoice_num;//发票号
-private  Integer butr_state;//交易状态，0表示交易中，1表示交易结束
+private Integer butr_state;//交易状态，0表示交易中，1表示交易结束
 private BusNeed busNeed;//班车需求fk
 @Id
 @GeneratedValue(strategy=GenerationType.AUTO)
