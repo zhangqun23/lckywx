26bd9abef36e696c8ddfc1257da539fc149f3ab4
zhangqun23 <2287961949@qq.com>
2017-08-17 15:27:07 +0800

    张群班车查询

--- WebContent/js/app/busNeed.js
+++ WebContent/js/app/busNeed.js
@@ -65,14 +65,13 @@ app.run([ '$rootScope', '$location', function($rootScope, $location) {
 
 // 路由配置
 app.config([ '$routeProvider', function($routeProvider) {
-	$routeProvider.when('/busNeed', {
+	$routeProvider.when('/busNeedIndex', {
 		templateUrl : '/lckywx/jsp/busNeed/busNeed.html',
 		controller : 'PlatformController'
 	}).when('/busNeedInfo/:busneed', {
 		templateUrl : '/lckywx/jsp/busNeed/busNeedInfo.html',
 		controller : 'BusNeedInfoController'
-	})
-	.when('/busNeedList', {
+	}).when('/busNeedList', {
 		templateUrl : '/lckywx/jsp/busNeed/busNeedList.html',
 		controller : 'PlatformController'
 	})
@@ -89,78 +88,91 @@ app.factory('services', [ '$http', 'baseUrl', function($http, baseUrl) {
 			data : data
 		});
 	};
-	
+	// zq查询班车定制需求
+	services.selectBusNeeds = function(data) {
+		return $http({
+			method : 'post',
+			url : baseUrl + 'busNeed/selectBusNeed.do',
+			data : data
+		});
+	};
+
 	return services;
 } ]);
-app
-		.controller(
-				'PlatformController',
-				[
-						'$scope',
-						'services',
-						'$location',
-						function($scope, services, $location) {
-							var busNeed = $scope;
-							busNeed.BusLimit={
-									bune_tel:"",
-									bune_num:"",
-									bune_time:"",
-									bune_gath_time:"",
-									bune_gath_pla:"",
-									bune_goal_pla:"",
-									bune_purp:"",
-									bune_remark:""
-							}
-							busNeed.addBusNeed=function(){
-								var busLimit = JSON
-								.stringify(busNeed.BusLimit);
-								services.addBusNeed({
-									busNeed : busLimit
-								}).success(function(data) {
-									
-									 $location.path("busNeedInfo/"+JSON.stringify(data.result));
-									if (data.result) {
-										alert("是");
-										/*console.log("zq"+JSON
-												.stringify(data.result));*/
-									} else {
-										alert("否");
-									}
-								});
-							}
-							busNeed.toProducer = function () { 
-								$location.path("#/busNeedInfo");
-							};
-							// zq初始化
-							function initData() {
-								console.log("初始化页面信息");
-								
-								if ($location.path().indexOf('/busNeed') == 0) {
-									
+app.controller('PlatformController', [
+		'$scope',
+		'services',
+		'$location',
+		function($scope, services, $location) {
+			var busNeed = $scope;
+			busNeed.BusLimit = {
+				bune_tel : "",
+				bune_num : "",
+				bune_time : "",
+				bune_gath_time : "",
+				bune_gath_pla : "",
+				bune_goal_pla : "",
+				bune_purp : "",
+				bune_remark : ""
+			}
+			// zq添加包车需求
+			busNeed.addBusNeed = function() {
+				var busLimit = JSON.stringify(busNeed.BusLimit);
+				services.addBusNeed({
+					busNeed : busLimit
+				}).success(
+						function(data) {
 
-								} else if ($location.path().indexOf(
-										'/busNeedInfo') == 0) {
-									var producerId = $stateParams.producerId;
-									alert(producerId);
-								} 
+							$location.path("busNeedInfo/"
+									+ JSON.stringify(data.result));
+							if (data.result) {
+								alert("是");
+								/*
+								 * console.log("zq"+JSON
+								 * .stringify(data.result));
+								 */
+							} else {
+								alert("否");
 							}
-							initData();
-						} ]);
-app
-.controller(
-		'BusNeedInfoController',
-		[
-				'$scope',
-				'services',
-				'$location',
-				'$routeParams',
-				function($scope, services, $location,$routeParams) {
-					
-					$scope.BNeed=JSON.parse($routeParams.busneed);
+						});
+			}
+			// zq查询班车需求列表
+			busNeed.selectBusNeeds = function() {
 				
-				} ]);
+				services.selectBusNeeds({
+					startDate : busNeed.startDate,
+					endDate : busNeed.endDate
+				}).success(function(data) {
+					busNeed.busNeedList = data.list;
+				});
+			}
+			// zq初始化
+			function initData() {
+				console.log("初始化页面信息");
+				if ($location.path().indexOf('/busNeedIndex') == 0) {
 
-//时间的格式化的判断
+				} else if ($location.path().indexOf('/busNeedList') == 0) {
+					alert("jinlaile");
+					var startDate = "2017-01-12";
+					var endDate = "2017-08-12";
+					services.selectBusNeeds({
+						startDate : startDate,
+						endDate : endDate
+					}).success(function(data) {
+						busNeed.busNeedList = data.list;
+					});
+				}
+			}
+			initData();
+		} ]);
+app.controller('BusNeedInfoController', [ '$scope', 'services', '$location',
+		'$routeParams', function($scope, services, $location, $routeParams) {
+
+			$scope.BNeed = JSON.parse($routeParams.busneed);
+
+		} ]);
+
+// 时间的格式化的判断
 app.filter('dateType', function() {
 	return function(input) {
 		console.log(input);
@@ -172,14 +184,14 @@ app.filter('dateType', function() {
 		return type;
 	}
 });
-//时间的格式化的判断
+// 时间的格式化的判断
 app.filter('isOrNotNull', function() {
 	return function(input) {
 		var type = "";
 		if (input) {
 			type = input;
-		}else{
-			type="无";
+		} else {
+			type = "无";
 		}
 
 		return type;
