6cbbd75fee09bdd71555f8888536a30d6741f654
ychengjiang <1358551974@qq.com>
2017-08-18 13:09:41 +0800

    Travel修改

--- src/com/mvc/repository/TravelRepository.java
+++ src/com/mvc/repository/TravelRepository.java
@@ -7,12 +7,7 @@
  */
 package com.mvc.repository;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-
 import com.mvc.entiy.Travel;
 
 
