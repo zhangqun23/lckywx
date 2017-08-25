34e612f92c91182d69115ae3ad6c01a5b14d439e
huiminjun <2802076526@qq.com>
2017-08-25 21:57:13 +0800

    王慧敏:更新openid

--- src/com/mvc/controller/LoginController.java
+++ src/com/mvc/controller/LoginController.java
@@ -18,7 +18,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.alibaba.fastjson.JSON;
 import com.mvc.service.LoginService;
+
+import net.sf.json.JSONObject;
 
 
 
--- src/com/mvc/controller/RouteController.java
+++ src/com/mvc/controller/RouteController.java
@@ -5,6 +5,7 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
+import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -16,9 +17,14 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.base.constants.ReportFormConstants;
 import com.mvc.entityReport.CheckHouse;
+import com.mvc.entiy.BusNeed;
+import com.mvc.entiy.User;
+import com.mvc.repository.BusNeedRepository;
+import com.mvc.repository.UserRepository;
 import com.mvc.service.CheckHouseService;
 import com.utils.CollectionUtil;
 import com.utils.CookieUtil;
+import com.utils.OpenidUtil;
 import com.utils.StringUtil;
 
 import net.sf.json.JSONObject;
@@ -31,12 +37,21 @@ import net.sf.json.JSONObject;
  */
 @Controller
 @RequestMapping("/routeController")
-public class RouteController {
-
+public class RouteController extends HttpServlet{
+	@Autowired
+	UserRepository userRepository;
 
 
 	@RequestMapping("/toPlatformPage.do")
 	public String InvoiceReceivePage() {
+		String str0=getInitParameter("appid");
+		String code=getInitParameter("response_type");
+		System.out.println("慧敏君来了"+str0);
+		OpenidUtil.getOpenid(null, null, code);
+		User user=new User();
+		user.setUser_openId(OpenidUtil.getOpenid(null, null, code));
+		User result = userRepository.saveAndFlush(user);
+		
 		return "platform/index"; 
 	}
 
